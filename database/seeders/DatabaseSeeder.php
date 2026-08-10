cfcc4a06e6ab9dfb5ead61b2e11d6cb05e222f7e
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-08-09 22:19:24 -0300

    Chave estrageira de categoria em produto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            ProdutoSeeder::class,
             CategoriaSeeder::class,
+            ProdutoSeeder::class,
         ]);
     }
 }
--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Produto;
+use App\Models\Categoria;
 
 class ProdutoSeeder extends Seeder
 {
@@ -13,10 +14,13 @@ class ProdutoSeeder extends Seeder
      */
     public function run(): void
     {
+        $categoria = Categoria::find(1);
+
         Produto::create([
             'Nome' => "Notebook",
             'Preco' => 7000.50,
             'Quantidade' => 12,
+            'categoria_id' => $categoria->id,
         ]);
     }
 }
