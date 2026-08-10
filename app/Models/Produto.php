212b1508997a50ee2a6e8cb36d73465311f302ed
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-08-10 20:21:20 -0300

    Fillable

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    //Atributos que podem ser preenchidos por mass assigment
+    protected $fillable = [
+        'Nome',
+        'Preco',
+        'Quantidade',
+    ];
 }
