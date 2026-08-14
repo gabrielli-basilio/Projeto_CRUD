6e4dac7dfb8c962554c7751e26057c821f26e268
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-08-13 21:55:30 -0300

    Atualização de cardinalidades chave estrangeira

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Categoria extends Model
 {
-    //
+    public function produtos(): HasMany{
+        return $this->hasMany(Produto::class);
+    }
 }
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Produto extends Model
 {
@@ -13,4 +14,9 @@ class Produto extends Model
         'Quantidade',
         'categoria_id'
     ];
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
