20572202a0f69505b2f4f5fbbce03db22a7a6fa6
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-08-17 19:40:59 -0300

    Criação de Rotas, para CRUD

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,13 @@ use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('produtos');
 });
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
+
+// Rotas de CRUD
+
+Route::get('/produtos/create', [ProdutoController::class, 'create']);
+
+Route::post('/produtos', [ProdutoController::class, 'store']);
